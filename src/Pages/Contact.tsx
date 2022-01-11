363b82510285539d52b8b13f71372cff3338964f
umesh <rajputumesh117@gmail.com>
2022-01-12 02:36:52 +0530

    shop and product

---
+++ src/Pages/Contact.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+const Contact: FC = () => {
+  return (
+    <div>
+      <h1>Contact Component</h1>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,46 @@
+import React, { FC } from "react";
 import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
-const Header = () => {
-    return (
-        <Navbar bg="light" expand={false}>
-            <Container fluid>
-                <Navbar.Brand href="#">My Shop</Navbar.Brand>
-                <Navbar.Toggle aria-controls="offcanvasNavbar" />
-                <Navbar.Offcanvas
-                id="offcanvasNavbar"
-                aria-labelledby="offcanvasNavbarLabel"
-                placement="end"
-                >
-                <Offcanvas.Header closeButton>
-                    <Offcanvas.Title id="offcanvasNavbarLabel">My Shop</Offcanvas.Title>
-                </Offcanvas.Header>
-                <Offcanvas.Body>
-                    <Nav.Item className="justify-content-end flex-grow-1 pe-3">
-                        <NavLink className="nav-link" to="/">Home</NavLink>
-                    </Nav.Item>
-                    <Nav.Item className="justify-content-end flex-grow-1 pe-3">
-                        <NavLink className="nav-link" to="/about">About</NavLink>
-                    </Nav.Item>
-                    <Nav.Item className="justify-content-end flex-grow-1 pe-3">
-                        <NavLink className="nav-link" to="/contact">Contact</NavLink>
-                    </Nav.Item>
-                </Offcanvas.Body>
-                </Navbar.Offcanvas>
-            </Container>
-        </Navbar>
-    )
-}
+const Header: FC = () => {
+  return (
+    <>
+      <Navbar bg="light" expand={false}>
+        <Container>
+          <Navbar.Brand href="#">My Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="offcanvasNavbar" />
+          <Navbar.Offcanvas
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel">
+                My Shop
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav.Item className="justify-content-end flex-grow-1 pe-3">
+                <NavLink className="nav-link" to="/">
+                  Home
+                </NavLink>
+              </Nav.Item>
+              <Nav.Item className="justify-content-end flex-grow-1 pe-3">
+                <NavLink className="nav-link" to="/about">
+                  About
+                </NavLink>
+              </Nav.Item>
+              <Nav.Item className="justify-content-end flex-grow-1 pe-3">
+                <NavLink className="nav-link" to="/contact">
+                  Contact
+                </NavLink>
+              </Nav.Item>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
-export default Header
+export default Header;
